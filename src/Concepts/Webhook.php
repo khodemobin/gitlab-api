a5f41ae8ba417e4d1bb9b64847f776e178561838
mobin <mobinyazdanparast9@gmail.com>
2020-12-03 15:08:18 +0330

    fixing

--- src/Concepts/Webhook.php
+++ src/Concepts/Webhook.php
@@ -6,20 +6,32 @@ use GuzzleHttp\Client;
 
 trait Webhook
 {
+
+
     public static function webhooks($token, $projectId)
     {
         $client = new Client();
-        $response = $client->get(config('gitlab.gitlab_url') . "projects/{$projectId}/hooks", [
-            'headers' => [
-                'Authorization' => 'Bearer' . $token
-            ]
-        ])->getBody();
+        $response = $client->get(config('gitlab.gitlab_url') . "projects/{$projectId}/hooks?access_token={$token}")->getBody();
 
         return json_decode($response->getContents(), true);
     }
 
-    public static function createWebHook($token, $name)
+    public static function createWebHook($token, $projectId, $url, $enableSSL, $trigger = [], $branchFilter = "")
     {
         $client = new Client();
+
+        $formParams = [
+            'id' => $projectId,
+            'url' => $url,
+            'enable_ssl_verification' => $enableSSL,
+            'push_events_branch_filter' => $branchFilter
+        ];
+
+
+        $response = $client->post(config('gitlab.gitlab_url') . "projects/{$projectId}/hooks?access_token={$token}", [
+            'form_params' => array_merge($formParams, $trigger)
+        ])->getBody();
+
+        return json_decode($response->getContents(), true);
     }
 }
